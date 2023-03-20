1d4daa183a62e9bbc1a9b8b5d0bd5b58a7bda118
Akashchandra62 <akashchandra8544@gmail.com>
2023-03-20 14:47:13 +0530

    updated the slider in leftBar

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Leftbar = () => {
+  const [img, changeImg] = useState("1");
   return (
     <div
       className="w-100 pt-5"
@@ -18,33 +19,25 @@ const Leftbar = () => {
           eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
         </p>
         <div className="mt-4 d-flex gap-2">
-          <div
-            className=" rounded-pill"
-            style={{
-              width: "35px",
-              height: "5px",
-              backgroundColor: "rgba(77, 77, 144, 1)",
-            }}
-          ></div>
-          <div
-            className=" rounded-pill"
-            style={{
-              width: "35px",
-              height: "5px",
-              backgroundColor: "rgba(77, 77, 144, 1)",
-            }}
-          ></div>
-          <div
-            className="bg-white rounded-pill"
-            style={{
-              width: "35px",
-              height: "5px",
-            }}
-          ></div>
+          {[1, 2, 3].map((id) => {
+            return (
+              <div
+                key={id}
+                className=" rounded-pill"
+                style={{
+                  width: "35px",
+                  height: "5px",
+                  backgroundColor: img == id ? "white" : "rgba(77, 77, 144, 1)",
+                  cursor: "pointer"
+                }}
+                onClick = {(e)=>{ changeImg(id) }}
+              ></div>
+            );
+          })}
         </div>
       </div>
       <div className="w-100">
-        <img src="./images/2.png" alt="logo"></img>
+        <img src={`./images/${img}.png`} alt="logo"></img>
       </div>
     </div>
   );
